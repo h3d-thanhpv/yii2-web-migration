7377c64e80d2b3c296b90085cbf54489001bab37
thanhpv <thanhpv@fimo.edu.vn>
2016-05-29 10:51:37 +0700

    Remove unused code

--- config/web.php
+++ config/web.php
@@ -38,14 +38,6 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
     ],
     'params' => $params,
 ];
